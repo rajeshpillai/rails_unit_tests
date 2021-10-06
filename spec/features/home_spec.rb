ccff156e6d86490e8f888f2e696def011a1bab19
Rajesh Pillai <rajesh.algorisys@ext.idfy.com>
2021-10-06 11:37:44 +0530

    Fix home controller and capybara spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Hello world', type: :system do
+describe 'Home Page', type: :system do
   describe 'index page' do
     it 'shows the right content' do
       visit home_index_path
