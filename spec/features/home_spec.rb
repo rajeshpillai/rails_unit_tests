7c28f50df17d1a525cac33ef1247541e0618972d
Rajesh Pillai <rajesh.algorisys@ext.idfy.com>
2021-10-06 11:53:34 +0530

    Test root route

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -6,5 +6,12 @@ describe 'Home Page', type: :system do
       visit home_index_path
       expect(page).to have_content('Awesome Ecommerce')
     end
+
+    it '/ should render home/index view' do
+      visit "http://localhost:3000/"
+      expect(page.current_path).to eq '/'
+      expect(page).to have_content('Awesome Ecommerce')
+    end
+
   end
 end
